be1b30a58b7a28fb92b70810be0852da26951d84
gaileliovson <gail@airarena.net>
2022-03-10 12:54:24 +1100

    Update invoice_item.rb

--- lib/myob/api/models/invoice_item.rb
+++ lib/myob/api/models/invoice_item.rb
@@ -5,6 +5,12 @@ module Myob
         def model_route
           'Sale/Invoice/Item'
         end
+
+        # Added to Render sale invoices as PDF
+        def get(object, params)
+          @client.connection.get(self.url(object, params), {:headers => @client.headers})
+        end
+        
       end
     end
   end
